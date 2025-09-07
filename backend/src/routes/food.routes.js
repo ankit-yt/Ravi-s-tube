92331fa54636dfbe26fa089603962cb182f7f26f
ankit-yt <ankitpk3303@gmail.com>
2025-09-07 13:08:34 +0530

    first

---
+++ backend/src/routes/food.routes.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const foodController = require("../controllers/food.controller")
+const authMiddleware = require("../middlewares/auth.middleware")
+const router = express.Router();
+const multer = require('multer');
+
+
+const upload = multer({
+    storage: multer.memoryStorage(),
+})
+
+
+/* POST /api/food/ [protected]*/
+router.post('/',
+    authMiddleware.authFoodPartnerMiddleware,
+    upload.single("mama"),
+    foodController.createFood)
+
+
+/* GET /api/food/ [protected] */
+router.get("/",
+    foodController.getFoodItems)
+
+
+router.post('/like',
+    authMiddleware.authUserMiddleware,
+    foodController.likeFood)
+
+
+router.post('/save',
+    authMiddleware.authUserMiddleware,
+    foodController.saveFood
+)
+
+
+router.get('/save',
+    authMiddleware.authUserMiddleware,
+    foodController.getSaveFood
+)
+
+
+
+module.exports = router
