ef3b1cb43807d5eb28317e374f9827df89b042de
ankit-yt <ankitpk3303@gmail.com>
2025-09-07 13:21:34 +0530

    cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://ravi-s-tube.vercel.app",
     credentials: true
 }));
 app.use(cookieParser());
